cd3a30a4d450577a35efd56e3b8e4d5101705a38
Henrique Cruz <eu@henriquecruz.com.br>
2020-12-31 15:44:27 -0300

    Min PHP version warning

---
+++ includes/views/notices/html-notice-min-php-version.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Admin View: Notice - WooCommerce missing.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
+
+<div class="error">
+	<p><strong><?php _e( 'Temos um problema!', 'woocommerce-boleto' ); ?></strong> <?php echo __( 'Sua versão do PHP é anterior a v5.6. Atualize para que o plugin da Paghiper funcione corretamente.', 'woocommerce-boleto' ); ?></p>
+</div>
