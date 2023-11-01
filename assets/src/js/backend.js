c36ab6ce03f30e577388392ab0198085a954946c
Henrique Cruz <eu@henriquecruz.com.br>
2023-11-01 04:52:47 -0300

    jQuery Mask safe check before initializing

--- assets/src/js/backend.js
+++ assets/src/js/backend.js
@@ -33,6 +33,8 @@ jQuery(document).ready( function($){
 	});
 
 	// Provides maskable fields for date operations
-	$(".date").mask("00/00/0000", {placeholder: "__/__/____", clearIfNotMatch: true});
+	if(typeof $.fn.mask === 'function') {
+		$(".date").mask("00/00/0000", {placeholder: "__/__/____", clearIfNotMatch: true});
+	}
 
 });
