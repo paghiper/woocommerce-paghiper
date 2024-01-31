b2522dccb3923a1f9a5c1e824d77156aaa1bc381
Henrique Cruz <eu@henriquecruz.com.br>
2024-01-31 15:04:18 -0300

    Get Order Billing City using proper Order Class Method

--- includes/class-wc-paghiper-transaction.php
+++ includes/class-wc-paghiper-transaction.php
@@ -282,7 +282,7 @@ class WC_PagHiper_Transaction {
 		$data['payer_complement']  	= $this->order->get_billing_address_2();
 		$data['payer_district']		= $this->order->billing_neighborhood;
 		$data['payer_number']	 	= $this->order->billing_number;
-		$data['payer_city']       	= $this->order->billing_city;
+		$data['payer_city']       	= $this->order->get_billing_city();
 		$data['payer_state']      	= $this->order->get_billing_state();
 		$data['payer_zip_code']   	= $this->order->get_billing_postcode();
 
